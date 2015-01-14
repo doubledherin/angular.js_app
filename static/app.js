03a5520c7fa98695514f9ba79b9d3e2dc7f8c05d
Wendy Dherin <wendydherin@gmail.com>
2015-01-14 12:41:04 -0800

    Wrap entire app in a closure.

--- static/app.js
+++ static/app.js
@@ -1,2 +1,15 @@
-/* The below line is used by the ng-app="store" attribute on the html element of index.html to create an app on document load. 'angular' is the Angular library; 'module' is a method that creates a module; the method's first argument (a string) is the module's name; the second (a list; now empty) will store the module's dependencies.*/
+/* It's a good habit to wrap your entire app in a closure.*/
+(function(){
+
+/* Used by <html ng-app="store"> in index.html to create app on document load. 
+'angular' is the Angular library; 'module' is a method that creates a module; the method's first argument (a string) is the module's name; the second (a list; now empty) will store the module's dependencies.*/
 var app = angular.module('store', [ ]);
+
+/* By convention, first argument (name of controller, a string) should be a camelcase and should include the word 'controller' and reference it's module. 
+Second argument is an anonymous function that gets executed when StoreController is called.*/
+app.controller('StoreController', function(){
+
+});
+
+
+})();
